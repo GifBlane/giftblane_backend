f146027fb4a645eac225cf5e0ab208140e59e3ca
IMatfres <matt.estunlp@gmail.com>
2022-12-15 19:58:56 -0300

    feat(ligin): add endpoint login POST

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { loginUser } = require('../controllers/auth/loginController');
+
+const router = express.Router()
+
+router.post('/auth/login', loginUser)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { get } = require('../controllers/index')
+const authRoutes = require('./authRoutes')
 
 const router = express.Router()
 
 // example of a route with index controller get function
-router.get('/', get)
+router.use("/", authRoutes);
 
 module.exports = router
