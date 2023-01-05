4d339830798c6f90fb0ff91f4c6b21742f4bb001
HectorSalgadoM <94656754+HectorxSM317@users.noreply.github.com>
2023-01-05 17:24:24 -0300

    [PINS] Endpoint getAllPins (#12)
    * create pinsRouter
    
    * pin controllers
    
    * created createPin controller
    
    * create getAllPins controller
    
    Co-authored-by: Luis Solano <luisjavier2012@gmail.com>

---
+++ controllers/Pines/getAllPins.js
@@ -0,0 +1,29 @@
+const createHttpError = require('http-errors');
+const { Pin } = require('../../database/models');
+const { endpointResponse } = require('../../helpers/success');
+const { catchAsync } = require('../../helpers/catchAsync');
+
+module.exports = {
+    getAllPins: catchAsync(async(req, res, next) => {
+        //TODO: add paginate
+        try {
+            const pins = await Pin.findAll()
+            endpointResponse({
+                res,
+                message: 'Users  retrieved successfully',
+                body: pins,
+            });
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error retrieving Pins] - [getPins - GET]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
+
+
+}
+
+
+
