6f99b53c82e1def60993649059df2cfbcf4eb92a
sasuske <jelver00@hotmail.com>
2023-01-05 08:23:32 -0500

    create deleteUser controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-//Controllers
+//Controllers Users
 const { getAllUsers } = require('../controllers/Users/GetAllUsers');
 const { createUser } = require('../controllers/Users/CreateUsers');
 const { updateUser } = require('../controllers/Users/UpdateUsers');
 const { getUserById } = require('../controllers/Users/getByIdUsers');
+const { deleteUser } = require('../controllers/Users/deleteUser');
 
 //Middlewares
 const { userExist } = require('../middlewares/userExist');
@@ -12,9 +13,13 @@ const { userExist } = require('../middlewares/userExist');
 const router = express.Router();
 
 // example of a route with index controller get function
+//Routes Users
 router.get('/users', getAllUsers);
 router.post('/users', createUser);
 router.get('/users/:id', getUserById);
 router.patch('/users/:id', userExist, updateUser);
+router.delete('/users/:id', deleteUser);
+
+//Routes Pines
 
 module.exports = router;
