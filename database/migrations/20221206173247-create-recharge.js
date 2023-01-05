d38af0a5a4b2ddc6c14351dfc9c16956b83b0a65
sasuske <jelver00@hotmail.com>
2023-01-05 15:46:33 -0500

    create seeder recharges

--- database/migrations/20221206173247-create-recharge.js
+++ database/migrations/20221206173247-create-recharge.js
@@ -1,45 +1,45 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Recharges', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      dateCreate: {
-        type: Sequelize.DATE
-      },
-      idUserCreated: {
-        type: Sequelize.INTEGER
-      },
-      idComerce: {
-        type: Sequelize.INTEGER,
-        references: {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Recharges', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			dateCreate: {
+				type: Sequelize.DATE,
+			},
+			idUserCreated: {
+				type: Sequelize.INTEGER,
+			},
+			idComerce: {
+				type: Sequelize.INTEGER,
+				/* references: {
           model: 'Commerces',
           key: 'id'
         },
         onDelet: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      value: {
-        type: Sequelize.FLOAT
-      },
-      numCard: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Recharges');
-  }
+        onUpdate: 'CASCADE' */
+			},
+			value: {
+				type: Sequelize.FLOAT,
+			},
+			numCard: {
+				type: Sequelize.INTEGER,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Recharges');
+	},
 };
--- database/seeders/20230105135344-demo-pin.js
+++ database/seeders/20230105135344-demo-pin.js
@@ -21,6 +21,33 @@ module.exports = {
 				createdAt: new Date(),
 				updatedAt: new Date(),
 			},
+			{
+				swap: 'prueba2',
+				typeCode: 'n02',
+				status: 'active',
+				code: 'c02',
+				typeCard: 'f02',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				swap: 'prueba3',
+				typeCode: 'n03',
+				status: 'active',
+				code: 'c03',
+				typeCard: 'f03',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				swap: 'prueba4',
+				typeCode: 'n04',
+				status: 'active',
+				code: 'c04',
+				typeCard: 'f04',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
 		]);
 	},
 	down: (queryInterface, Sequelize) => {
