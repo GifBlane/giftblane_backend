c3b3829c09dbb1a05737dfb1ea4396c3753f451d
jair <10952706+Jair-Academlo@users.noreply.github.com>
2023-01-05 07:49:40 -0500

    [USERS] Endpoint GetByIdUsers (#9)
    * created controller updateUsers
    
    * update user controller
    
    * create controller update user
    
    * create controller getByIdUsers
    
    * updated changes getUsersById controller

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+
+//Controllers
 const { getAllUsers } = require('../controllers/Users/GetAllUsers');
 const { createUser } = require('../controllers/Users/CreateUsers');
+const { updateUser } = require('../controllers/Users/UpdateUsers');
+const { getUserById } = require('../controllers/Users/getByIdUsers');
+
+//Middlewares
+const { userExist } = require('../middlewares/userExist');
 
 const router = express.Router();
 
 // example of a route with index controller get function
 router.get('/users', getAllUsers);
 router.post('/users', createUser);
+router.get('/users/:id', getUserById);
+router.patch('/users/:id', userExist, updateUser);
 
 module.exports = router;
