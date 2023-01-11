5318d0dd350c5b42843135201242e71fb04e039e
LucasVaroneW <81322848+LucasVaroneW@users.noreply.github.com>
2023-01-10 21:35:41 -0300

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,18 @@ const { updateUser } = require('../controllers/Users/UpdateUsers');
 const { getUserById } = require('../controllers/Users/getByIdUsers');
 const { deleteUser } = require('../controllers/Users/deleteUser');
 
-//comments
+//Recharges Controller
+const { createRecharge } = require('../controllers/Recharge/createRecharge')
+const { getAllRecharge } = require('../controllers/Recharge/getAllRecharge')
+const { getRechargeById } = require('../controllers/Recharge/RechargeById')
 
 //Pins Controllers
 const { createPin } = require('../controllers/Pines/createPin');
 const { getAllPins } = require('../controllers/Pines/getAllPins');
 const { updatePin } = require('../controllers/Pines/updatePin');
-const { deletePin } = require('../controllers/Pines/deletePin');
-const { getByIdPin } = require('../controllers/Pines/getByIdPin');
 
-
+//Settings Controllers
+const { createSettings } = require('../controllers/Settings/setting')
 
 //Middlewares
 const { userExist } = require('../middlewares/userExist');
@@ -37,19 +39,7 @@ router.delete('/users/:id', deleteUser);
 
 router.post('/pins', createPin)
 router.get('/pins', getAllPins)
-router.get('/pins/:id([0-9]+)', getByIdPin)
-router.delete('/pins/delete/:id([0-9]+)', deletePin)
-router.patch('/pins/:id([0-9]+)', updatePin)
-
-
-//Routes Recharge
-router.post('/recharge', createRecharge)
-router.get('/recharge', getAllRecharge)
-router.patch('/recharge/:id', getRechargeById)
-
-//Routes Commerce
-
-router.get('/commerce', getAllCommerces)
+router.patch('/pins/:id', updatePin)
 
 //Routes Recharge
 router.post('/recharge', createRecharge)
