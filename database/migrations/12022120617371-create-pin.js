aff9d555cdcdd1899189154e1fcfd7751df6a278
jair <10952706+Jair-Academlo@users.noreply.github.com>
2023-01-19 20:07:07 -0500

    update config createPines controller (#34)

--- database/migrations/12022120617371-create-pin.js
+++ database/migrations/12022120617371-create-pin.js
@@ -1,39 +1,48 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Pins', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      swap: {
-        type: Sequelize.STRING
-      },
-      typeCode: {
-        type: Sequelize.STRING
-      },
-      status: {
-        type: Sequelize.STRING
-      },
-      code: {
-        type: Sequelize.STRING
-      },
-      typeCard: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Pins');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Pins', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			typeCard: {
+				type: Sequelize.ENUM('nemo', 'black'),
+				allowNull: false,
+			},
+			code: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			status: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			typeCode: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				defaultValue: 'QR',
+			},
+			swap: {
+				type: Sequelize.ENUM('buy', 'game'),
+			},
+			dateVigencia: {
+				allowNull: true,
+				type: Sequelize.DATE,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Pins');
+	},
 };
